54940df8cec10b520f8695f5b71b95c83dd6488f
Stan Domula <stan.domula@lovoo.com>
2017-12-05 16:43:16 +0100

    clean up

--- main.go
+++ main.go
@@ -1,57 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"strings"
-
-	"github.com/joho/godotenv"
-	"github.com/kelseyhightower/envconfig"
-	"github.com/sirupsen/logrus"
+	"log"
 )
 
 func main() {
-	// Load env-file if it exists first
-	if env := os.Getenv("PLUGIN_ENV_FILE"); env != "" {
-		godotenv.Load(env)
-	}
-
-	var p Plugin
-	if err := envconfig.Process("plugin", &p); err != nil {
-		logrus.WithError(err).Fatal("failed to parse parameters")
-	}
-	if p.ShowEnv {
-		for _, e := range os.Environ() {
-			pair := strings.Split(e, "=")
-			fmt.Println(pair[0])
-		}
-	}
-	if p.Debug {
-		logrus.SetLevel(logrus.DebugLevel)
-	}
-	if err := preparePlugin(&p); err != nil {
-		logrus.WithError(err).Fatal("failed to prepare plugin")
+	p, err := newPlugin()
+	if err != nil {
+		log.Fatalf("could not initialize helm plugin: %v", err)
 	}
 
 	if err := p.Exec(); err != nil {
-		logrus.WithError(err).Fatal("failed to execute plugin")
+		log.Fatalf("failed to execute helm plugin: %v", err)
 	}
-}
-
-func preparePlugin(p *Plugin) error {
-	if p.Package == "" {
-		s := strings.Split(p.ChartPath, "/")
-		p.Package = s[len(s)-1]
-	}
-	if p.Release == "" {
-		p.Release = p.Package
-	}
-	if p.ChartRepo == "" && p.Bucket != "" {
-		p.ChartRepo = fmt.Sprintf("https://%s.storage.googleapis.com/", p.Bucket)
-	}
-	if p.Namespace == "" {
-		p.Namespace = "default"
-	}
-
-	return nil
 }
